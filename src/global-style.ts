e6ce416e56414402bc72354a367ad67617e00a79
David Costa <davidcostadev@gmail.com>
2020-02-23 09:29:34 -0300

    Add Owner Filter

--- src/global-style.ts
+++ src/global-style.ts
@@ -2,6 +2,31 @@ import { createGlobalStyle } from 'styled-components'
 import { FontWeights, Colors } from './lib/style-guide'
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://rsms.me/inter/inter.css');
+
+  html { font-family: 'Inter', sans-serif; }
+  @supports (font-variation-settings: normal) {
+    html { font-family: 'Inter var', sans-serif; }
+  }
+
+  :root {
+    --white: #ffffff;
+    --text-normal: #192533;
+    --text-placeholder: #8299B6;
+    --text-secondary: #60789A;
+    --primary10: #F5F9FF;
+    --primary20: #DEECFC;
+    --primary30: #D1E3F8;
+    --primary100: #3284E1;
+  }
+
+  ::placeholder {
+    color: var(--text-placeholder);
+  }
+  ::-webkit-input-placeholder {
+    color: var(--text-placeholder);
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
@@ -11,7 +36,6 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body, input {
-    font-family: proxima-nova, sans-serif;
     font-weight: ${FontWeights.PR};
     font-size: 14px;
     line-height: 1.4rem;
